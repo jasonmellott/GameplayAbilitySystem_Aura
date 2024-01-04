1f8fc972c163cc87dbe244b2eb3b53b3ddb673a6
KrymsonVerse <jasonmellott@gmail.com>
2024-01-04 11:46:16 -0800

    Game Mode

---
+++ Source/Aura/Public/Game/AuraGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 2024 WAM Productions
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AuraGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
