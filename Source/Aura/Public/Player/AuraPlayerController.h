4f931c3ac942fe84cc30a0e7bf31188eb1bb0a8c
KrymsonVerse <jasonmellott@gmail.com>
2024-01-03 22:50:20 -0800

    Enhanced Input

---
+++ Source/Aura/Public/Player/AuraPlayerController.h
@@ -0,0 +1,17 @@
+// Copyright 2024 WAM Productions
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "AuraPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
