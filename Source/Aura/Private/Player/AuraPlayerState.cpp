53c167de513aca7a140cca66f52bab58cf5ec415
KrymsonVerse <jasonmellott@gmail.com>
2024-01-05 12:04:50 -0800

    Player State

---
+++ Source/Aura/Private/Player/AuraPlayerState.cpp
@@ -0,0 +1,9 @@
+// Copyright 2024 WAM Productions
+
+
+#include "Player/AuraPlayerState.h"
+
+AAuraPlayerState::AAuraPlayerState()
+{
+	NetUpdateFrequency = 100.f;
+}
---
+++ Source/Aura/Public/Player/AuraPlayerState.h
@@ -0,0 +1,19 @@
+// Copyright 2024 WAM Productions
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "AuraPlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+public:
+	AAuraPlayerState();
+	
+};
