7486f95c72cf7b835ff4b9573d8935b8a02366a3
KrymsonVerse <jasonmellott@gmail.com>
2024-01-06 13:24:47 -0800

    User Widget and Widget Controller

---
+++ Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,10 @@
+// Copyright 2024 WAM Productions
+
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
