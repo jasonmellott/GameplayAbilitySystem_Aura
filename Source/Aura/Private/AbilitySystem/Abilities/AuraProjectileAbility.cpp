4f5dec182261a5cd0e064863ddf753664a1d89e8
KrymsonVerse <jasonmellott@gmail.com>
2024-01-22 16:16:57 -0800

    Spawn Firebolt from event

--- Source/Aura/Private/AbilitySystem/Abilities/AuraProjectileAbility.cpp
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraProjectileAbility.cpp
@@ -8,8 +8,11 @@
 void UAuraProjectileAbility::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
 {
 	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
-	
-	const bool bIsServer = HasAuthority(&ActivationInfo);
+}
+
+void UAuraProjectileAbility::SpawnProjectile()
+{
+	const bool bIsServer = GetAvatarActorFromActorInfo()->HasAuthority();
 	if (!bIsServer) return;
 
 	ICombatInterface* CombatInterface = Cast<ICombatInterface>(GetAvatarActorFromActorInfo());
@@ -22,7 +25,7 @@ void UAuraProjectileAbility::ActivateAbility(const FGameplayAbilitySpecHandle Ha
 		// TODO: Set the projectile rotation
 
 		AAuraProjectile* Projectile = GetWorld()->SpawnActorDeferred<AAuraProjectile>(ProjectileClass, SpawnTransform, GetOwningActorFromActorInfo(), Cast<APawn>(GetOwningActorFromActorInfo()), ESpawnActorCollisionHandlingMethod::AlwaysSpawn);
-		
+
 		//TODO: Give the projectile a gameplay effect spec for causing damage
 
 		Projectile->FinishSpawning(SpawnTransform);
